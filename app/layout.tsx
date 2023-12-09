9f37cc8c1eac05c3cd99dea402a01669067e122e
ranjsidhu <ranjeetsidhu7433@gmail.com>
2023-12-09 15:27:57 +0000

    web vitals

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Analytics from "./Analytics";
+import reportWebVitals from "./reportWebVitals";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,3 +23,5 @@ export default function RootLayout({
     </html>
   );
 }
+
+reportWebVitals();
