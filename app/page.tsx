fce3d21590b7c0cbd4e3d2dbc9bd979ebef89f5d
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2023-12-29 16:39:40 +0000

    Update description in metadata

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Homepage from "./home/Homepage";
 export const metadata: Metadata = {
   title: "Home | Tutoring To Success",
   description:
-    "Elevate your child's education with Tutoring To Success in Wolverhampton. Dedicated to providing top-quality educational experiences for GCSE, A-Level, KS1/KS2, and 11+. Join us for expert tutoring, personalized learning, and proven results—whether in-person or virtually through Zoom.",
+    "Elevate your child's education with Tutoring To Success in Wolverhampton, Willenhall, Wednesfield,  Tettenhall, Newbridge and the surrounding areas. Dedicated to providing top-quality educational experiences for GCSE, A-Level, KS1/KS2, and 11+. Join us for expert tutoring, personalized learning, and proven results—whether in-person or virtually through Zoom.",
   alternates: {
     canonical: "https://tutoringtosuccess.co.uk",
     types: {
