4177a6c1bed97336152846089c018b92b2ff3461
ranjsidhu <ranjeetsidhu7433@gmail.com>
2025-01-08 13:17:01 +0000

    chore: resolve vulnerabilities

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: { unoptimized: true },
+  output: "standalone",
 };
 
 module.exports = nextConfig;
