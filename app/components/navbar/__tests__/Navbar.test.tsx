73c7d38998931a51652b88f500de4063e80b1f5b
ranjsidhu <ranjeetsidhu7433@gmail.com>
2025-04-16 19:22:50 +0100

    test: navbar unit tests

--- app/components/navbar/__tests__/Navbar.test.tsx
+++ app/components/navbar/__tests__/Navbar.test.tsx
@@ -14,6 +14,7 @@ jest.mock("next/navigation", () => ({
 jest.mock("next/image", () => ({
   __esModule: true,
   default: (props: any) => {
+    // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
     return <img {...props} />;
   },
 }));
